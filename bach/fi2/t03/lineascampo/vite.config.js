4fe2e85abd18256151ff2e6d1286060d86751897
onio72 <antoniogonzalezgarcia@gmail.com>
2026-01-22 15:08:50 +0100

    Configure Pages build for lineascampo

--- bach/fi2/t03/lineascampo/vite.config.js
+++ bach/fi2/t03/lineascampo/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/iesmajuelo/bach/fi2/t03/lineascampo/',
+  build: {
+    outDir: '../../../../docs/bach/fi2/t03/lineascampo',
+    emptyOutDir: true,
+  },
 })
